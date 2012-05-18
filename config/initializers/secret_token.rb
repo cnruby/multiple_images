53c955207362f314faec27fafcbf783fd6943c19
Gudao Luo <gudao.luo@gmail.com>
2012-05-18 22:51:28 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultipleImages::Application.config.secret_token = 'c3b14daca1f133c57a4a3f9d2c587e46f170001191dd5a3e4b1115b9b8ec59cc7572a76047f8fff8551184391c28595acc623bb54ba7dc418b9774e35f72980f'

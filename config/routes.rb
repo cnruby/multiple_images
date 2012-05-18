e62ba1d79ab668a1bb42dcdccd33ea2779dd7ce2
Gudao Luo <gudao.luo@gmail.com>
2012-05-18 23:41:24 +0200

    project cloud testing uncomplete

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ MultipleImages::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'blogs'
 
   # See how all your routes lay out with "rake routes"
 
